c4f91253848d71bea2eec165ecc590c823b3f9e3
John Macintosh <jeffersonclemente.70@gmail.com>
2021-12-10 22:38:27 +0800

    Added Documentation

--- checker.py
+++ checker.py
@@ -1,12 +1,31 @@
+"""
+    OMR (Optical Mark Recognition) Bubble Sheets Using OpenCV Python
+    Authors:
+        JUSTINE BALDOVINO
+        JEFFERSON CLEMENTE
+        EXEQUIEL LUSTAN
+        JHOSUA MOSTAR
+    A requirement for CS Elective Computer Vision BSCS-4C.
+"""
 import cv2 as cv
 import numpy as np
 import os
+import detect_name
 
 original_answer_key_image = []
 original_answer_sheet_image = []
 
 
 def load_images(answer_sheet_path, answer_key_path):
+    """
+        @param answer_sheet_path - Answer Sheets Folder Path
+        @param answer_key_path - Answer Key File Path
+
+        This load_images function loads all necessary images needed for processing
+        and all images loaded are resized to 1200 x 1500.
+
+        Returns Answer Sheet Images (Array), Answer Key Image
+    """
     global original_answer_key_image, original_answer_sheet_image
     collected_answer_sheets = []
 
@@ -25,6 +44,17 @@ def load_images(answer_sheet_path, answer_key_path):
 
 
 def preprocess_image(answer_sheets, answer_key):
+    """
+        @param answer_sheets - Collected Answer Sheets Images
+        @param answer_key - Answer Key Image
+
+        The preprocess_image function will make premilinary processes
+        to the image for further image processing (contour detection).
+        The premilinary processes are:
+            GRAYSCALING , BLURING , CANNY EDGE DETECTION.
+
+        Returns Preprocessed Answer Sheets (Array), Preprocessed Answer Key
+    """
     processed_answer_sheets = []
 
     gray = cv.cvtColor(answer_key, cv.COLOR_BGR2GRAY)
@@ -41,6 +71,15 @@ def preprocess_image(answer_sheets, answer_key):
 
 
 def test_checker(processed_answer_sheet, processed_answer_key):
+    """
+        @param processed_answer_sheet - Preprocessed answer sheet images.
+        @param processed_answer_key - Preprocessed answer key.
+
+        The function test_checker will now perform FUNCTION CALLS for the checking of
+        preprocessed images of answer sheet and answer key. Both images are CROPPED
+        based on THREE columns of the two image to find the bubbles and passed to
+        get_answer_keys and check_answer_sheet functions.
+    """
     global original_answer_key_image, original_answer_sheet_image
 
     correct_answers_list = get_answer_keys(
@@ -71,6 +110,17 @@ def test_checker(processed_answer_sheet, processed_answer_key):
 
 
 def get_answer_keys(answer_key, original_key):
+    """
+        @param answer_key - Preprocessed Answer Key Image.
+        @param original_key - Original Answer Key Image.
+
+        The get_answer_keys function performs:
+            CROPPING - to remove uncessesary features in the image for contour detection,
+            CONTOUR DETECTION
+            FILTER CONTOURS - for finding bubbles in the image and shaded bubbles as correct answers.
+
+        Returns Correct Answers List (Array)
+    """
 
     firstCol = crop_image(answer_key, 80, 300, answer_key.shape[1], 300)
     secCol = crop_image(answer_key, 450, 300, answer_key.shape[1], 300)
@@ -100,11 +150,32 @@ def get_answer_keys(answer_key, original_key):
 
 
 def crop_image(image, x_coord, y_coord, height, width):
+    """
+        @param image - Image Source/File
+        @param x_coord - X coordinate in image Pixel
+        @param y_coord - Y coordinate in image Pixel
+        @param height - Height size of crop image
+        @param width - Width size of crop image
 
+        The crop_image function performs basic image CROPPING.
+
+        Returns Cropped Image
+    """
     return image[y_coord:y_coord+height, x_coord:x_coord+width]
 
 
 def get_bubbles(image):
+    """
+        @param image - Image Source/File
+
+        The get_bubbles functions performs THRESHOLDING and CONTOUR DETECTION.
+        CONTOURS are sorted using the BOUNDING RECT of itself filtering based on
+        WIDTH and HEIGHT.
+        FILTERED CONTOURS are sorted using functions sort() and
+        get_contour_precedence()
+
+        Returns Sorted Bubbles (Array)
+    """
     thresh = cv.threshold(image, 100, 255,
                           cv.THRESH_OTSU)[1]
     cnts, _ = cv.findContours(thresh, cv.RETR_EXTERNAL,
@@ -122,20 +193,43 @@ def get_bubbles(image):
 
 
 def get_contour_precedence(contour, cols):
-    tolerance_factor = 10
+    """
+        @param contour - Image contours
+        @param cols - Image height
+
+        The get_contour_precedence function is used to sorts the contours from left-to-right
+        and top-to-bottom by calculating based on X and Y coordinates of contours with a
+        tolerance factor.
+
+        Returns (numeric value)
+    """
+    tolerance_factor = 70
     origin = cv.boundingRect(contour)
     return ((origin[1] // tolerance_factor) * tolerance_factor) * cols + origin[0]
 
 
 def get_shaded(answer_key_contours, original_answer_key_image, x_coord, y_coord):
+    """
+        @param answer_key_contours - contours of bubble answer sheet image
+        @param original_answer_key_image - answer key image
+        @param x_coord - x coordinate in image pixel
+        @param y_coord - y coordinate in image pixel
+
+        The get_shaded function performs filtering of shaded bubbles by using
+        boundingRect(), cv.mean() to get average color value in an certain are of image using
+        the return values of bounding rect and filtered according to its values, adding a
+        rectangle marker to the image.
+
+        Returns marked original image, corrects answers index list (Array)
+    """
     correct_answers = []
 
     for i in range(len(answer_key_contours)):
         x, y, w, h = cv.boundingRect(answer_key_contours[i])
         x = x + x_coord
         y = y + y_coord
-        cv.putText(original_answer_key_image, str(i), (x, y),
-                   cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
+        # cv.putText(original_answer_key_image, str(i), (x, y),
+        #           cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
         BGRs = np.array(
             cv.mean(original_answer_key_image[y:y+h, x:x+w])).astype(np.uint8)
         if BGRs[0] < 210:
@@ -147,7 +241,16 @@ def get_shaded(answer_key_contours, original_answer_key_image, x_coord, y_coord)
 
 
 def check_answer_sheet(croppped_image, original_answer_sheet_image, correct_answers, x_coord, y_coord):
+    """
+        @param cropped_image - cropped image source/file
+        @param original_answer_sheet_image - original answer sheet image/file
+        @param correct_answers - correct answers index list
 
+        The function check_answer_sheet performs function calls to get_bubbles() and
+        check_shaded().
+
+        Returns Marked Answer Sheet, Correct Answers (Numeric Value)
+    """
     answer_sheet_bubbles = get_bubbles(croppped_image)
 
     processed_image, correct_answers = check_shaded(
@@ -157,6 +260,20 @@ def check_answer_sheet(croppped_image, original_answer_sheet_image, correct_answ
 
 
 def check_shaded(contours, original_image, correct_answers, x_coord, y_coord):
+    """
+        @param contours - image contours
+        @param original_image - original image
+        @param correct_answers - correct answers list
+        @param x_coord - x coordinate in image pixel
+        @param y_coord - y coordinate in image pixel
+
+        The check_shaded function uses boundingRect() and cv.mean() function for checking
+        the answer sheets to find and check color average color values within the region in the image.
+        Using B values in (BGR) it determines if the region is shaded or not. If shaded it matches to
+        the correct answers list if it has same index.
+
+        Returns checked answer sheet image, correct answers (Numeric)
+    """
     correct = 0
     for i in range(len(contours)):
         x, y, w, h = cv.boundingRect(contours[i])
@@ -179,6 +296,15 @@ def check_shaded(contours, original_image, correct_answers, x_coord, y_coord):
 
 
 def plot_score(imS, correct, items, n):
+    """
+        @param imS - image source/file
+        @param correct - total correct score
+        @param items - total items in answer sheet
+        @param n - counter
+
+        The plot_score function writes total score in the answer sheet image.
+        Using putText() function.
+    """
     average = int(items*0.75)
     if correct < average:
         cv.putText(imS, str(correct), (600, 140),
@@ -190,6 +316,9 @@ def plot_score(imS, correct, items, n):
                cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
     cv.putText(imS, str(items), (600, 175),
                cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
+
+    # detect_name here
+
     cv.imshow("outsput" + str(n), imS)
 
 
